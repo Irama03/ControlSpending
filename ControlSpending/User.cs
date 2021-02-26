f90b3dd85cf6d395b0c7e4578a650164146d20bd
Andriienko Yelyzaveta <yelyzaveta.andriienko@ukma.edu.ua>
2021-02-26 17:43:47 +0200

    Changes in class User and creating tests for User

--- ControlSpending/User.cs
+++ ControlSpending/User.cs
@@ -4,9 +4,43 @@ namespace ControlSpending
 {
     public class User
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Ira. It's Liza!");
+        private string _name;
+        private string _surname;
+        private string _email;
+
+        public string Name 
+        { 
+            get { return _name; } 
+            set { _name = value; } 
+        }
+
+        public string Surname 
+        { 
+            get { return _surname; } 
+            set { _surname = value; } 
+        }
+
+        public string Email 
+        { 
+            get { return _email; } 
+            set { _email = value; } 
+        }
+
+        public string FullName 
+       {
+            get
+            {
+                var result = Surname;
+                if (!String.IsNullOrWhiteSpace(Name))
+                {
+                    if (!String.IsNullOrWhiteSpace(Surname))
+                    {
+                        result += ", ";
+                    }
+                    result += Name;
+                }
+                return result;
+            }
         }
     }
 }
---
+++ ControlSpendingTests/UserTest.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace ControlSpendingTests
+{
+    public class UserTest
+    {
+
+        [Fact]
+        public void FullNameTestValid()
+        {
+            var user = new User();
+
+
+        }
+    }
+}

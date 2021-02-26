34cd8f4c84c147add8053d6b946325dd47662d7e
Iryna_Matviienko <irama05@outlook.com>
2021-02-26 18:12:37 +0200

    First changes to Wallet and Category

--- ControlSpending/Category.cs
+++ ControlSpending/Category.cs
@@ -1,7 +1,40 @@
 ﻿namespace ControlSpending
 {
+    //Категорія складається з назви, опису, кольору та іконки
     public class Category
     {
+        private string _name;
+        private string _description;
+        private string _color;
+        private string _icon;
+
+        public string Name
+        {
+            get { return _name; } 
+            set { _name = value; } 
+        }
+
+        public string Description
+        {
+            get { return _description; } 
+            set { _description = value; } 
+        }
+
+        public string Color
+        {
+            get { return _color; } 
+            set { _color = value; }
+        }
+
+        public string Icon
+        {
+            get { return _icon; } 
+            set { _icon = value; }
+        }
         
+        public override string ToString()
+        {
+            return $"{Name}, {Description}, {Color}, {Icon}";
+        }
     }
 }
--- ControlSpending/Wallet.cs
+++ ControlSpending/Wallet.cs
@@ -1,7 +1,42 @@
 ﻿namespace ControlSpending
 {
+    //Гаманець. Повинен зберігати назву, початковий баланс гаманцю, опис та
+    //основну валюту. До нього додаються транзакції. Для кожного гаманця можна
+    //вказати список категорій, який доступний в ньому для транзакцій.
     public class Wallet
     {
+        private string _name;
+        private double _initialBalance;
+        private string _description;
+        private string _mainCurrency;
         
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+        public double InitialBalance
+        {
+            get { return _initialBalance; }
+            set { _initialBalance = value; }
+        }
+        
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        
+        public string MainCurrency
+        {
+            get { return _mainCurrency; }
+            set { _mainCurrency = value; }
+        }
+        
+        public override string ToString()
+        {
+            return $"{Name}, {InitialBalance}, {Description}, {MainCurrency}";
+        }
     }
 }
